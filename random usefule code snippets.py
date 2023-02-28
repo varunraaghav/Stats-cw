e91582da4d889b47b30f62f9ced0b8dbef06e001
Varun Raaghav <varun.raaghav20@imperial.ac.uk>
2023-02-28 01:08:42 +0000

    initialised data

---
+++ random usefule code snippets.py
@@ -0,0 +1,12 @@
+# HOW TO DO SUBPLOTS (IE PLOTS SIDE BY SIDE) 
+
+# fig,ax = plt.subplots(ncols=2)
+
+# ax[0].plot(X, Y, '.')
+# ax[0].set_title('blue dots')
+
+
+# ax[1].plot(X, Y, 'x', color='red')
+# ax[1].set_title('red x')
+
+# ______________________________________________________________________________________________
--- statistics_cw.py
+++ statistics_cw.py
@@ -3,5 +3,24 @@ import numpy as np
 import pandas as pd
 import random
 import scipy
+import matplotlib.pyplot as plt
 
 from sklearn.preprocessing import PolynomialFeatures
+
+random.seed(1847960)
+
+df = pd.read_csv('vrv20.csv')
+
+X = df['X'].values.tolist()
+Y = df['Y'].values.tolist()
+
+plt.figure(dpi=150)
+plt.plot(X, Y, '.')
+plt.title('vrv20: Wavlength (nm) plotted against time')
+plt.grid(True)
+
+# _______ import data into 2 lists
+
+
+
+plt.show()
