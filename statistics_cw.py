cf5fd53a594b90af387cf2412f97ef1c8c4a8fc2
Varun Raaghav <varun.raaghav20@imperial.ac.uk>
2023-02-28 17:01:37 +0000

    exercise 2c completed

--- statistics_cw.py
+++ statistics_cw.py
@@ -12,13 +12,12 @@ from sklearn.linear_model import LinearRegression
 
 from scipy import stats
 
-# ___________________________________________________________________
-
+# _______________________________________________________________________________________________________________________
 # Random seed
 random.seed(1847960)
-# ___________________________________________________________________
+# _______________________________________________________________________________________________________________________
+# Importing and reading wavelength data from the designated csv. Performing prerequisite numpy operations to format data
 
-# Importing and reading wavelength data from the designated csv
 df = pd.read_csv('vrv20.csv')
 
 
@@ -27,6 +26,12 @@ Y_list = df['Y'].values.tolist()
 
 x = np.array(X_list)  # converting list into numpy arrays for future manipulations
 y = np.array(Y_list)
+
+x = x.reshape(-1, 1)  # reshaping the arrays retains the original data but changes the shape of the structure to be easily used in future operations
+y = y.reshape(-1, 1)
+
+# _______________________________________________________________________________________________________________________
+
 
 # Histogram plot of the wavelngth data 
 
@@ -42,6 +47,8 @@ y = np.array(Y_list)
 # plt.ylabel('Wavelength (nm)')
 # plt.title("Wavlength Boxplot")
 
+# _______________________________________________________________________________________________________________________
+
 
 
 # Present Statistic values in a table form (using pd table)
@@ -49,7 +56,7 @@ y = np.array(Y_list)
 mean = np.mean(y)
 trimmed_mean = stats.trim_mean(y, 0.1)
 median = np.median(y)
-std_dev = np.std(y)
+std_dev = np.std(y, ddof=0)      # This is the population standard deviation, different from sample standard deviation
 q1, q3 = np.percentile(y, [25, 75])
 iqr = q3 - q1
 
@@ -60,41 +67,91 @@ table_data = {
 }
 table = pd.DataFrame(table_data)
 print(table)
+# _____________________________________________________________________________________________________________________
 
 
-# Simple linear regression model of the wavelngth data
 
-poly_simple = PolynomialFeatures(degree=1, include_bias=False)
-x_reshaped = x.reshape(-1, 1)
-y_reshaped = y.reshape(-1, 1)
+# Plotting Unstandardised Scatter plot for Wavelength vs time: USE IN REPORT
+# 
+# plt.figure(dpi=150)
+# plt.plot(x, y, '.', color = 'green')
+# plt.ylabel('Wavelength (nm)')
+# plt.xlabel('Time')
+# plt.title('UNSTANDARDISED: Wavlength (nm) plotted against time')
+# plt.grid(True)
+# 
+# _______________________________________________________________________________________________________________________
 
-x_poly_simple = poly_simple.fit_transform(x_reshaped)
-model_simple = LinearRegression().fit(x_poly_simple, y_reshaped)
-y_predicted_simple = model_simple.predict(x_poly_simple)
 
-# linear regression with polynomial term (ie k = 2)
+
+# Standardising the time index: X
+
+mean_x = np.mean(x)
+std_dev_x_sample = np.std(x, ddof=1)  # calculating sample mean instead of population mean
+x_standardised = (x - mean_x) / std_dev_x_sample
+
+# _____________________________________________________________________________________________________________________
+
+
+
+# # Simple linear regression model of the wavelength data
+
+# poly_simple = PolynomialFeatures(degree=1, include_bias=False)
+
+# x_poly_simple = poly_simple.fit_transform(x)
+# model_simple = LinearRegression().fit(x_poly_simple, y)
+# y_predicted_simple = model_simple.predict(x_poly_simple)
+
+# # _______________________________________________________________________________________________________________________
+
+
+
+
+# # linear regression with polynomial term (ie k = 2)
 
 poly_2 = PolynomialFeatures(degree=2, include_bias=False)
 
-x_poly_2 = poly_2.fit_transform(x_reshaped)
-model_2 = LinearRegression().fit(x_poly_2, y_reshaped)
+x_poly_2 = poly_2.fit_transform(x)
+model_2 = LinearRegression().fit(x_poly_2, y)
 y_predicted_2 = model_2.predict(x_poly_2)
+
+# _______________________________________________________________________________________________________________________
 
 
 # scatter plot of the wavelength data , with the linear regression values (exercise 2)
 plt.figure(dpi=150)
-plt.plot(x, y, '.')
+plt.plot(x_standardised, y, '.')
 plt.ylabel('Wavelength (nm)')
-plt.xlabel('Time')
+plt.xlabel('Time (Standardised)')
 plt.title('vrv20: Wavlength (nm) plotted against time')
 
-plt.plot(x_reshaped, y_predicted_simple, color = 'red', label='k=1 (Simple) linear regression model') #simple linear regression
-plt.plot(x_reshaped, y_predicted_2, color = 'green', label='k = 2 (qudratic) linear regression model') #quadrating linear regression
+
+
+# Calculating Linear regression y_predicted values in a for-loop, to the kth order: 
+# when i = 1, the 'simple' linear fit is made. when i = 2, the 'qudratic fit' is made, and the order of the polynomial increases when i increases
+
+k = 5     # determines the max order upto which the linear regression y_predicted values are stored
+linear_regression_values_df = pd.DataFrame()   # all predicted values are stored in columns in a pandas dataframe, so that the values can be easily accessed for future operations
+linear_regression_values_df['x_standardised'] = x_standardised.tolist()
+
+
+for i in range (1, k+1):
+    poly_k = PolynomialFeatures(degree=i, include_bias=False)
+    x_poly_k = poly_k.fit_transform(x)
+    model_k = LinearRegression().fit(x_poly_k, y)
+    y_predicted_k = model_k.predict(x_poly_k)
+
+    linear_regression_values_df[str(i)] = y_predicted_k.tolist()
+
+    plt.plot(x_standardised, y_predicted_k, label='Order ' + str(i))
 
 
 plt.legend()
 plt.grid(True)
 
+# _______________________________________________________________________________________________________________________
+
 
 # Show the necessary graphs at the end (commented out once the code is assumed to be working)
 plt.show()
+
